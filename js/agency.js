de05bea027fed0da154e37daf9e150d7c421b8ba
Steve Ross <steve@oak.works>
2015-09-18 18:43:09 -0400

    added home page animation on what we do and started 'services' section

--- js/agency.js
+++ js/agency.js
@@ -36,6 +36,23 @@ $(function() {
     }else{
         $('html, body').show();
     }
+
+
+
+        $("#typed").typed({
+            stringsElement: $('#typed-strings'),
+            // strings: ["Typed.js is a <strong>jQuery</strong> plugin.", "It <em>types</em> out sentences.", "And then deletes them.", "Try it out!"],
+
+            typeSpeed: 70,
+            backDelay: 500,
+            startDelay: 1000,
+            loop: false,
+            contentType: 'html', // or text
+            // defaults to false for infinite loop
+            loopCount: false,
+        });
+
+
 });
 
 
